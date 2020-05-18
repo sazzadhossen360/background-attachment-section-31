a2e62c40d0a9d6e6d3899ef35efdffc4f35046ca
sazzad hossen <sazzadhossen2544@gmail.com>
2020-05-18 11:16:41 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        $('#parallax-1').parallax({
+            naturalWidth: 600,
+            naturalHeight: 400
+          });
+    });
+    
+    jQuery(window).load(function(){
+        
+    });
+}(jQuery));	
